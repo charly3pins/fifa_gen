b38997318c03c4f5485faf0c550d69b6b05f9b1f
charly <charlyfb90@gmail.com>
2020-04-16 16:00:21 +0200

    refactor :hammer:: added a bit of style to form

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -31,125 +31,154 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   Widget _buildForm(BuildContext context, AuthNotifier authNotif) {
-    return SingleChildScrollView(
-        child: Column(
-      children: <Widget>[
-        Container(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                _authMode == AuthMode.SIGNUP
-                    ? TextFormField(
-                        decoration: InputDecoration(
-                            labelText: "Name", hasFloatingPlaceholder: true),
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter your Name.';
-                          }
-                          return null;
-                        },
-                        onSaved: (val) =>
-                            setState(() => _user.name = val.trim()),
-                      )
-                    : Container(),
-                SizedBox(height: 20),
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "Username", hasFloatingPlaceholder: true),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter your Username.';
-                    }
-                    return null;
-                  },
-                  onSaved: (val) => setState(() => _user.username = val.trim()),
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "Password", hasFloatingPlaceholder: true),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter a Password.';
-                    }
-                    return null;
-                  },
-                  obscureText: true,
-                  onSaved: (val) => setState(() => _user.password = val.trim()),
-                ),
-                SizedBox(height: 20),
-                FlatButton(
-                  child: _authMode == AuthMode.LOGIN
-                      ? Text("Log in")
-                      : Text("Sign up"),
-                  color: Color(0xFF4B9DFE),
-                  textColor: Colors.white,
-                  onPressed: () {
-                    final form = _formKey.currentState;
-                    if (form.validate()) {
-                      form.save();
-                      _authMode == AuthMode.LOGIN
-                          ? authNotif.logIn(_user)
-                          : authNotif.signUp(_user);
-                      form.reset();
-                      _user = User();
-                    }
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
-        _authMode == AuthMode.LOGIN
-            ? Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
+    return Container(
+        color: Color.fromRGBO(36, 43, 47, 1),
+        padding: const EdgeInsets.symmetric(horizontal: 43.0),
+        child: Form(
+          key: _formKey,
+          child: Container(
+            alignment: Alignment.center,
+            child: SingleChildScrollView(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  SizedBox(height: 40),
-                  Text(
-                    "Don't have an account?",
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      setState(() {
-                        _authMode = AuthMode.SIGNUP;
-                        authNotif.clearError();
-                      });
+                  _authMode == AuthMode.SIGNUP
+                      ? TextFormField(
+                          decoration: _buildInputDecoration("Name"),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your Name.';
+                            }
+                            return null;
+                          },
+                          style: TextStyle(
+                              color: Color.fromRGBO(252, 252, 252, 1),
+                              fontFamily: 'RadikalLight'),
+                          onSaved: (val) =>
+                              setState(() => _user.name = val.trim()),
+                        )
+                      : Container(),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    decoration: _buildInputDecoration("Username"),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter your Username.';
+                      }
+                      return null;
                     },
-                    textColor: Colors.black,
-                    child: Text("Create an account"),
-                  )
-                ],
-              )
-            : Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(height: 40),
-                  Text(
-                    "Already have an account?",
-                    style: TextStyle(color: Colors.grey),
+                    style: TextStyle(
+                        color: Color.fromRGBO(252, 252, 252, 1),
+                        fontFamily: 'RadikalLight'),
+                    onSaved: (val) =>
+                        setState(() => _user.username = val.trim()),
                   ),
-                  FlatButton(
-                    onPressed: () {
-                      setState(() {
-                        _authMode = AuthMode.LOGIN;
-                        authNotif.clearError();
-                      });
+                  SizedBox(height: 20),
+                  TextFormField(
+                    decoration: _buildInputDecoration("Password"),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter a Password.';
+                      }
+                      return null;
                     },
-                    textColor: Colors.black,
-                    child: Text("Log in to your account"),
-                  )
+                    obscureText: true,
+                    style: TextStyle(
+                        color: Color.fromRGBO(252, 252, 252, 1),
+                        fontFamily: 'RadikalLight'),
+                    onSaved: (val) =>
+                        setState(() => _user.password = val.trim()),
+                  ),
+                  SizedBox(height: 20),
+                  RaisedButton(
+                    child: Text(
+                      _authMode == AuthMode.LOGIN ? "Log in" : "Sign up",
+                      style: TextStyle(
+                          color: Color.fromRGBO(40, 48, 52, 1),
+                          fontFamily: 'RadikalMedium',
+                          fontSize: 14),
+                    ),
+                    color: Colors.white,
+                    elevation: 4.0,
+                    onPressed: () {
+                      final form = _formKey.currentState;
+                      if (form.validate()) {
+                        form.save();
+                        _authMode == AuthMode.LOGIN
+                            ? authNotif.logIn(_user)
+                            : authNotif.signUp(_user);
+                        form.reset();
+                        _user = User();
+                      }
+                    },
+                  ),
+                  _authMode == AuthMode.LOGIN
+                      ? Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SizedBox(height: 40),
+                            Text(
+                              "Not a member?",
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                            FlatButton(
+                              onPressed: () {
+                                setState(() {
+                                  _authMode = AuthMode.SIGNUP;
+                                  authNotif.clearError();
+                                });
+                              },
+                              textColor: Colors.white,
+                              child: Text("Sign up"),
+                            )
+                          ],
+                        )
+                      : Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SizedBox(height: 40),
+                            Text(
+                              "Already a member?",
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                            FlatButton(
+                              onPressed: () {
+                                setState(() {
+                                  _authMode = AuthMode.LOGIN;
+                                  authNotif.clearError();
+                                });
+                              },
+                              textColor: Colors.white,
+                              child: Text("Log in"),
+                            )
+                          ],
+                        ),
+                  Consumer<AuthNotifier>(
+                    builder: (context, authNotif, child) =>
+                        SnackBarLauncher(error: authNotif.error),
+                  ),
                 ],
               ),
-        Consumer<AuthNotifier>(
-          builder: (context, authNotif, child) =>
-              SnackBarLauncher(error: authNotif.error),
-        ),
-      ],
-    ));
+            ),
+          ),
+        ));
   }
+
+  InputDecoration _buildInputDecoration(String hint) {
+  return InputDecoration(
+      focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Color.fromRGBO(252, 252, 252, 1))),
+      hintText: hint,
+      enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Color.fromRGBO(151, 151, 151, 1))),
+      hintStyle: TextStyle(color: Color.fromRGBO(252, 252, 252, 1)),
+      //icon: iconPath != '' ? Image.asset(iconPath) : null,
+      errorStyle: TextStyle(color: Color.fromRGBO(248, 218, 87, 1)),
+      errorBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color:  Color.fromRGBO(248, 218, 87, 1))),
+      focusedErrorBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color:  Color.fromRGBO(248, 218, 87, 1))));
+}
 }
