01354f4fb81798735f40753b9e809632f4ad507a
charly <charlyfb90@gmail.com>
2020-04-23 23:54:46 +0200

    feat :sparkles:: group endpoints

--- lib/core/services/api.dart
+++ lib/core/services/api.dart
@@ -8,9 +8,13 @@ import 'package:http/http.dart' as http;
 
 class Api {
   static const _baseUrl = 'http://10.0.2.2:8000';
+  // static const _baseUrl = 'http://192.168.1.59:8000';
 
   var client = new http.Client();
 
+  // ***********************************************
+  // ****************** USERS **********************
+  // ***********************************************
   Future<User> logIn(User user) async {
     print("Api => logIn");
     String jsonBody = json.encode(user.toJson());
@@ -59,6 +63,9 @@ class Api {
     return results;
   }
 
+  // *************************************************
+  // ******************* FRIENDS *********************
+  // *************************************************
   Future<List<User>> findFriends(String userID, String filter) async {
     // TODO add catch and return INTERNAL ERROR if the API is down
     var response = await client.get(
@@ -112,7 +119,7 @@ class Api {
   }
 
   Future<Friendship> getFriendship(String userID, String otherUserID) async {
-    print("Api => updateFriendship");
+    print("Api => getFriendship");
     // TODO add catch and return INTERNAL ERROR if the API is down
     var response = await client.get(
         "$_baseUrl/users/$userID/friendships/$otherUserID",
@@ -124,6 +131,52 @@ class Api {
     }
 
     throw ("Error getting friendship"); // TODO create constant
+  }
+
+  // *************************************************
+  // ******************* GROUPS **********************
+  // *************************************************
+  Future<Group> createGroup(Group group) async {
+    print("Api => createGroup");
+    String jsonBody = json.encode(group.toJson());
+    // TODO add catch and return INTERNAL ERROR if the API is down
+    var response = await client.post("$_baseUrl/groups",
+        headers: {"Content-Type": 'application/json'}, body: jsonBody);
+
+    if (response.statusCode == 200) {
+      final parsed = json.decode(response.body);
+      return Group.fromJson(parsed);
+    }
+    print("API => ERROR");
+    throw ("Error creating group"); // TODO create constant
+  }
+
+  Future updateGroup(Group group) async {
+    print("Api => updateGroup");
+    String jsonBody = json.encode(group.toJson());
+    // TODO add catch and return INTERNAL ERROR if the API is down
+    var response = await client.put("$_baseUrl/groups/${group.id}",
+        headers: {"Content-Type": 'application/json'}, body: jsonBody);
+
+    if (response.statusCode == 200 && response.body.isEmpty) {
+      return null;
+    }
+    print("API => ERROR");
+    throw ("Error updating group"); // TODO create constant
+  }
+
+  Future<Group> getGroup(String id) async {
+    print("Api => getFriendship");
+    // TODO add catch and return INTERNAL ERROR if the API is down
+    var response = await client.get("$_baseUrl/groups/$id",
+        headers: {"Content-Type": 'application/json'});
+
+    if (response.statusCode == 200) {
+      final parsed = json.decode(response.body);
+      return Group.fromJson(parsed);
+    }
+
+    throw ("Error getting group"); // TODO create constant
   }
 
   Future<List<Group>> findGroups(String userID) async {
